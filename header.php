d42ddb5ded9b1c0162fa914095c6e047aca00d46
lollipas <draakonlohe123@gmail.com>
2019-12-29 22:59:19 +0200

    commit

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
         <ul class="menu">
             <li class="list" style="float:right"><a href="http://meloni-ilusalong.local/kontaktid-ja-asukoht/">Kontaktid ja asukoht</a></li>
             <li class="list" style="float:right"><a href="http://meloni-ilusalong.local/teenused/">Teenused</a></li>
-            <li class="list" style="float:right"><a href="http://meloni-ilusalong.local/galerii/">Galerii</a></li>
+           
             <li class="list" style="float:right"><a href="http://meloni-ilusalong.local/">Avaleht</a></li>
              <a href="#top">  
             <img src="https://i.imgur.com/uDfjbQD.png"  id="logo">
